c2f34534835a478ee091541d914bbb5f7867ed9a
linuxluemmel.ch <linuxluemmel.ch@14a9197e-7d63-2fe5-09fc-39de0cd1e8e0>
2010-07-01 09:22:25 +0000

    Add OS-Linux.py

---
+++ trunk/resources/lib/OS-Linux.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#########################################################
+# SCRIPT  : OS-Linux.py                                 #
+#########################################################
+# AUTHOR  : Hans Weber                                  #
+# EMAIL   : linuxluemmel.ch@gmail.com                   #
+# XBMC    : Version 10.5  or higher                     #
+# OS      : Linux                                       #
+# TASKS   : This python code contains only os-dependet  #
+#           functions and must be rewritten for very    #
+#           os that should exexcute this addon.         #
+# VERSION : 0.6C                                        #
+# DATE    : 06-27-10                                    #
+# STATE   : Alpha 1                                     #
+# LICENCE : GPL 3.0                                     #
+#########################################################
+#                                                       #
+# Project-Homepage :                                    #
+# http://code.google.com/p/swiss-army-knife/            #
+#                                                       #
+#########################################################
+
+
+
+
+####################### IMPORTS #########################
+
+import os, sys, thread, stat, time, string, re
+
+#########################################################
+
