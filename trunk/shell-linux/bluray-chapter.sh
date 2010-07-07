3b813418cbf8ed63828e8054e52f4bcdab0d7430
linuxluemmel.ch <linuxluemmel.ch@14a9197e-7d63-2fe5-09fc-39de0cd1e8e0>
2010-07-07 12:49:46 +0000

    r103

--- trunk/shell-linux/bluray-chapter.sh
+++ trunk/shell-linux/bluray-chapter.sh
@@ -17,9 +17,14 @@
 SCRIPTDIR="$HOME/.xbmc/addons/swiss-army-knife/shell-linux"
 
 
-echo -----------------------------------------------------------------
+echo
+echo --------------------------------------------------------------------
+SCRIPT=$(basename $0)
+echo "script  :" $SCRIPT
+cat version
 cd "$SCRIPTDIR" && echo changed to $SCRIPTDIR
-echo -----------------------------------------------------------------
+echo --------------------------------------------------------------------
+
 
 # Define the counting commands we expect inside the script
 
@@ -76,17 +81,8 @@ do
    fi
 done
 
-echo ----------------
-echo Clean temp files
-echo ----------------
 
 rm $HOME/.xbmc/userdata/addon_data/script-video-ripper/bluray/* >/dev/null 2>&1
-
-
-echo ---------------
-echo Toolchain found
-echo ---------------
-
 
 
 if [ $1 == '/dev/sr0' ] ; then
@@ -102,42 +98,22 @@ if [ $1 == '/dev/sr2' ] ; then
 fi
 
 
-echo ---------------------------------
-echo parameter for transcoder : $PARA
-echo ---------------------------------
-
-
 # We need the chapters (counting)
 
 chapter=$(makemkvcon info $PARA | grep '^Title' | grep -v skipped | wc -l)
 
 if [ $# -eq 0 ]; then
-  echo -------------------
-  echo "No Chapters found"
-  echo -------------------
   echo
+  echo ----------------------- script rc=3 -----------------------------
+  echo -----------------------------------------------------------------
   exit $E_NOCHAPERS
 fi
 
-echo ----------------------
-echo found $chapter Titles
-echo ----------------------
-
-echo -----------------------------------
-echo starting makemkvcon in stream-mode
-echo -----------------------------------
-
 nohup makemkvcon --messages=/dev/null stream $PARA & >/dev/null 2>&1
 
-echo -----------------------------------------
-echo wait 40 secounds until webserver is ready
-echo -----------------------------------------
 sleep 40.0
 
 lynx --dump  http://127.0.0.1:51000/web/titles > ~/.xbmc/userdata/addon_data/script-video-ripper/bluray/brmain.000
-echo --------------------------------------
-echo get titel info with the http-protocoll
-echo --------------------------------------
 max=`expr $chapter - 1`
 index=0
 while [ $chapter -gt $index ]
@@ -147,30 +123,17 @@ do
       index=`expr $index + 1`
 done
 
-echo -----------------------------------------
-echo Kill webserver
-echo -----------------------------------------
 
 kill -15 $(ps axu | grep makemkvcon | grep -v grep | awk '{print $2}') > /dev/null 2>&1
 
 
-echo -----------------------------------------
-echo get bluray volume-name
-echo -----------------------------------------
-
 VOLNAME=$(cat ~/.xbmc/userdata/addon_data/script-video-ripper/bluray/brmain.000 | grep name | tail -1 | awk '{print $2}' | tr -dc ‘[:alnum:]‘ )
 
 
-echo -----------------------------------------
-echo Volume-Name of bluray :[$VOLNAME]
 echo $VOLNAME > ~/.xbmc/userdata/addon_data/script-video-ripper/bluray/BR_VOLUME
-echo -----------------------------------------
 
 
 Tindex=0
-echo ------------------------------
-echo Generate track-list for script
-echo ------------------------------
 
 while [ $chapter -gt $Tindex ]
 do
@@ -187,11 +150,6 @@ do
     Tindex=`expr $Tindex + 1`
 done
 
-
-echo ------------------------------
-echo Generate Info GUI-Exec List
-echo ------------------------------
-
 LONGTRACK=$(cat $HOME/.xbmc/userdata/addon_data/script-video-ripper/media/BR_HELP | sort -r | head -1 | awk '{print $2}')
 LONGDURATION=$(cat $HOME/.xbmc/userdata/addon_data/script-video-ripper/media/BR_HELP | sort -r | head -1 | awk '{print $1}')
 
@@ -199,10 +157,9 @@ echo $LONGTRACK >> ~/.xbmc/userdata/addon_data/script-video-ripper/media/BR_GUI
 echo $LONGDURATION >> ~/.xbmc/userdata/addon_data/script-video-ripper/media/BR_GUI
 echo $VOLNAME >> ~/.xbmc/userdata/addon_data/script-video-ripper/media/BR_GUI
 
-echo --------------
-echo all jobs done
-echo --------------
-
+echo
+echo ----------------------- script rc=0 -----------------------------
+echo -----------------------------------------------------------------
 
 exit 0
 
