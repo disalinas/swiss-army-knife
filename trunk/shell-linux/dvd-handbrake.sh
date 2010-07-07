364e0de1a9082db672b8b8f0561c6b0e23972397
linuxluemmel.ch <linuxluemmel.ch@14a9197e-7d63-2fe5-09fc-39de0cd1e8e0>
2010-07-07 17:58:00 +0000

    r112

--- trunk/shell-linux/dvd-handbrake.sh
+++ trunk/shell-linux/dvd-handbrake.sh
@@ -25,9 +25,13 @@
 
 SCRIPTDIR="$HOME/.xbmc/addons/swiss-army-knife/shell-linux"
 
-echo -----------------------------------------------------------------
+echo
+echo --------------------------------------------------------------------
+SCRIPT=$(basename $0)
+echo "script  :" $SCRIPT
+cat version
 cd "$SCRIPTDIR" && echo changed to $SCRIPTDIR
-echo -----------------------------------------------------------------
+echo --------------------------------------------------------------------
 
 
 OUTPUT_ERROR="$HOME/.xbmc/userdata/addon_data/script-video-ripper/log/handbrake-error.log"
@@ -56,6 +60,8 @@ if [ $# -lt $EXPECTED_ARGS ]; then
   echo "p8,9  subtitle             -s 0 [0-X]"
   echo "dvd-handbrake.sh was called with wrong arguments"
   echo
+  echo ----------------------- script rc=1 -----------------------------
+  echo -----------------------------------------------------------------
   exit $E_BADARGS
 fi
 
@@ -63,6 +69,8 @@ fi
 if [ $4 -eq 0 ]; then
   echo "the parameter 4 must be starting with 1 !"
   echo
+  echo ----------------------- script rc=1 -----------------------------
+  echo -----------------------------------------------------------------
   exit $E_BADARGS
 fi
 
@@ -71,6 +79,7 @@ fi
 
 REQUIRED_TOOLS=`cat << EOF
 HandBrakeCLI
+strings
 sleep
 mencoder
 nohup
@@ -83,35 +92,19 @@ for REQUIRED_TOOL in ${REQUIRED_TOOLS}
 do
    which ${REQUIRED_TOOL} >/dev/null 2>&1
    if [ $? -eq 1 ]; then
-        echo ----------------------------------------------------------------------------
         echo "ERROR! \"${REQUIRED_TOOL}\" is missing. ${0} requires it to operate."
         echo "Please install \"${REQUIRED_TOOL}\"."
-        echo ----------------------------------------------------------------------------
         echo "ERROR! \"${REQUIRED_TOOL}\" is missing. ${0} requires it to operate." > $OUTPUT_ERROR
         echo "Please install \"${REQUIRED_TOOL}\"." > $OUTPUT_ERROR
+        echo
+        echo ----------------------- script rc=2 -----------------------------
+        echo -----------------------------------------------------------------
         exit $E_TOOLNOTF
    fi
 done
 
-echo ---------------
-echo Toolchain found
-echo ---------------
-
-
-echo ------------------
-echo Starting transcode
-echo ------------------
-
-
 if [ $# -eq 5 ]; then
     AUDIO1=$(($5 +  1))
-    echo -----------------------------------
-    echo 5 paramters $1 $2 $3 $4 $5
-    echo -----------------------------------
- 
-    echo --------------------------------------------
-    echo Send back current progress data to XBMC-GUI
-    echo --------------------------------------------
 
     echo 2 > ~/.xbmc/userdata/addon_data/script-video-ripper/progress/stages-counter
     echo "1 Pass 1/2 for transcoding" > ~/.xbmc/userdata/addon_data/script-video-ripper/progress/stages-descriptions
@@ -123,26 +116,42 @@ if [ $# -eq 5 ]; then
     -T -x ref=3:mixed-refs:bframes=6:b-pyramid=1:bime=1:b-rdo=1:weightb=1:analyse=all:8x8dct=1:subme=6:me=um h:merange=24:filter=-2,-2:ref=6:mixed-refs=1:trellis=1:no-fast-pskip=1:no-dct-decimate=1:direct=auto:cqm="dvd-handbrake-profile"  \
     -a $AUDIO1 -E ac3 &
 
-    echo ------------------------------
-    echo Handbrake has startet ...
-    echo ------------------------------
+    sleep 10
 
-    sleep 15
+    echo $$ > ~/.xbmc/userdata/addon_data/script-video-ripper/progress/progress-pid
+    ps axu | grep HandBrakeCLI | grep -v grep |awk '{print $2}' >> ~/.xbmc/userdata/addon_data/script-video-ripper/progress/progress-pid
+
+    echo processing data
+
     while [ 1=1 ];
-     do
-      PASS1=$(cat nohup.out | grep "task 1 of 2" | tail -1 | sed  's/,//g' | sed  's/%//g' | sed  's/(//g' | sed  's/)//g' | sed  's/\./ /g' | sed  's/\://g')
-      echo ....
-      echo $PASS1
-      echo ....
-      sleep 2
+    do
+      echo -n .
+      PASS1=$(strings nohup.out | tail -1 | grep Encoding | grep "1 of 2" | tail -1 | awk '{print $6}' | cut -d '.' -f1 )
+      PASS2=$(strings nohup.out | tail -1 | grep Encoding | grep "2 of 2" | tail -1 | awk '{print $6}' | cut -d '.' -f1 )
+      if [ -n "$PASS1" ] ; then
+         #  echo stage 1 [$PASS1] % completed
+         echo $PASS1 > ~/.xbmc/userdata/addon_data/script-video-ripper/progress/progress 
+         if [ $PASS1 -eq 99 ] ; then
+            sleep 5
+            echo 100 > ~/.xbmc/userdata/addon_data/script-video-ripper/progress/progress
+            sleep 1
+            echo 0 > ~/.xbmc/userdata/addon_data/script-video-ripper/progress/progress
+            echo 2 > ~/.xbmc/userdata/addon_data/script-video-ripper/progress/stages-current
+         fi
+      fi
+      if [ -n "$PASS2" ] ; then
+         # echo stage 2 [$PASS2] % completed
+         echo $PASS2 > ~/.xbmc/userdata/addon_data/script-video-ripper/progress/progress
+         if [ $PASS2 -eq 100 ] ; then
+             sleep 2
+             echo DONE > ~/.xbmc/userdata/addon_data/script-video-ripper/progress/progress-done
+             echo
+             echo processing data done
+             break
+         fi
+      fi
     done
 fi
-
-
-
-
-
-
 
 
 
@@ -150,16 +159,33 @@ if [ $# -eq 7 ]; then
     if [[ "$6" =~ ^-a ]] ; then
        AUDIO1=$(($5 +  1))
        AUDIO2=$(($7 +  1))
-       echo 7 parameters 2 audio  $1 $2 $3 $4 $5 $6 $7
-       HandBrakeCLI -i $1 /dev/sr0 -o $2/$3.mkv -t $4 -f mkv -m -S 1200 -e x264 -2 \
-       -T -x ref=3:mixed-refs:bframes=6:b-pyramid=1:bime=1:b-rdo=1:weightb=1:analyse=all:8x8dct=1:subme=6:me=um h:merange=24:filter=-2,-2:ref=6:mixed-refs=1:trellis=1:no-fast-pskip=1:no-dct-decimate=1:direct=auto:cqm="dvd-handbrake-profile" -a $AUDIO1,$AUDIO2 -A "Audio-1","Audio-2" -B auto,160 -R auto,auto -6 auto,auto -E ac3,acc
+
+       echo 2 > ~/.xbmc/userdata/addon_data/script-video-ripper/progress/stages-counter
+       echo "1 Pass 1/2 for transcoding" > ~/.xbmc/userdata/addon_data/script-video-ripper/progress/stages-descriptions
+       echo "2 Pass 2/2 for transcoding" >> ~/.xbmc/userdata/addon_data/script-video-ripper/progress/stages-descriptions
+       echo 1 > ~/.xbmc/userdata/addon_data/script-video-ripper/progress/stages-current
+       echo $2/$3.mkv > ~/.xbmc/userdata/addon_data/script-video-ripper/progress/progress-files
+
+       nohup HandBrakeCLI -i $1 /dev/sr0 -o $2/$3.mkv -t $4 -f mkv -m -S 1200 -e x264 -2 \
+       -T -x ref=3:mixed-refs:bframes=6:b-pyramid=1:bime=1:b-rdo=1:weightb=1:analyse=all:8x8dct=1:subme=6:me=um h:merange=24:filter=-2,-2:ref=6:mixed-refs=1:trellis=1:no-fast-pskip=1:no-dct-decimate=1:direct=auto:cqm="dvd-handbrake-profile" \
+       -a $AUDIO1,$AUDIO2 -A "Audio-1","Audio-2" -B auto,160 -R auto,auto -6 auto,auto -E ac3,acc &
+
+       sleep 10
+
+       echo $$ > ~/.xbmc/userdata/addon_data/script-video-ripper/progress/progress-pid
+       ps axu | grep HandBrakeCLI | grep -v grep |awk '{print $2}' >> ~/.xbmc/userdata/addon_data/script-video-ripper/progress/progress-pid
+
+
     fi
 fi
+
+
+
 
 if [ $# -eq 7 ]; then
     if [[ "$6" =~ ^-s ]] ; then
        AUDIO1=$(($5 + 1))
-       echo 7 parameters 1 audio 1 sub $1 $2 $3 $4 $5 $6 $7
+
        mencoder dvd://$4 -dvd-device $1 -ovc frameno -nosound -o /dev/null -sid $7 -vobsubout $2/$3
        HandBrakeCLI -i $1 /dev/sr0 -o $2/$3.mkv -t $4 -f mkv -m -S 1200 -e x264 -2 \
        -T -x ref=3:mixed-refs:bframes=6:b-pyramid=1:bime=1:b-rdo=1:weightb=1:analyse=all:8x8dct=1:subme=6:me=um h:merange=24:filter=-2,-2:ref=6:mixed-refs=1:trellis=1:no-fast-pskip=1:no-dct-decimate=1:direct=auto:cqm="dvd-handbrake-profile" -a $AUDIO1 -E ac3
@@ -169,14 +195,16 @@ fi
 if [ $# -eq 9 ]; then
      AUDIO1=$(($5 +  1))
      AUDIO2=$(($7 +  1))
-     echo 9 parameters $1 $2 $3 $4 $5 $6 $7 $8 $9
+
      mencoder dvd://$4 -dvd-device $1 -ovc frameno -nosound -o /dev/null -sid $9 -vobsubout $2/$3
      HandBrakeCLI -i $1 /dev/sr0 -o $2/$3.mkv -t $4 -f mkv -m -S 1200 -e x264 -2 \
      -T -x ref=3:mixed-refs:bframes=6:b-pyramid=1:bime=1:b-rdo=1:weightb=1:analyse=all:8x8dct=1:subme=6:me=um h:merange=24:filter=-2,-2:ref=6:mixed-refs=1:trellis=1:no-fast-pskip=1:no-dct-decimate=1:direct=auto:cqm="dvd-handbrake-profile" -a $AUDIO1,$AUDIO2 -A "Audio-1","Audio-2" -B auto,160 -R auto,auto -6 auto,dpl2 -E ac3,acc
 fi
 
 
-
+echo
+echo ----------------------- script rc=0 -----------------------------
+echo -----------------------------------------------------------------
 
 exit
 
