fcb36d6dbe526ec91ed4d5a466ad5cbf29d7e951
linuxluemmel.ch <linuxluemmel.ch@14a9197e-7d63-2fe5-09fc-39de0cd1e8e0>
2010-10-02 21:30:53 +0000

    user ffunctions can be used now

--- script.video.swiss.army.knife/resources/lib/Linux.py
+++ script.video.swiss.army.knife/resources/lib/Linux.py
@@ -2415,18 +2415,25 @@ def OSGetUserDesc(index):
 
     desc = []
 
+    UserScript = os.getenv("HOME") + '/swiss.army.knife/' + 'user' + str(index) + '.sh' 
     UserDescription = os.getenv("HOME") + '/swiss.army.knife/' + 'user' + str(index) + '.desc'
 
     if (__verbose__ == 'true'):
         OSlog("Try to load user-function description : " + UserDescription)
 
     if (os.path.exists(UserDescription)):
-        UserDescF = open(UserDescription,'r')
-        for line in UserDescF.readlines():
-            line = line.strip()
-            desc.append(line)
-        UserDescF.close
-        return desc[0]    
+
+        # We need both a description file and a shell-script
+
+        if (os.path.exists(UserScript)):
+            UserDescF = open(UserDescription,'r')
+            for line in UserDescF.readlines():
+                line = line.strip()
+                desc.append(line)
+            UserDescF.close
+            return desc[0]
+        else:
+            return " "    
     else:
         return " "
 #########################################################
