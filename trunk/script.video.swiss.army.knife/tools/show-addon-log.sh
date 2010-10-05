e896cb1a30926a8b9a8fd0ff63e54b37ee1a800e
linuxluemmel.ch <linuxluemmel.ch@14a9197e-7d63-2fe5-09fc-39de0cd1e8e0>
2010-10-05 00:40:32 +0000

    default dvd transcoding is now active

---
+++ trunk/script.video.swiss.army.knife/tools/show-addon-log.sh
@@ -0,0 +1,16 @@
+###########################################################
+# scriptname : show-addon-log.sh                          #
+###########################################################
+# This script is part of the addon swiss-army-knife for   #
+# xbmc and is licenced under the gpl-licence              #
+# http://code.google.com/p/swiss-army-knife/              #
+###########################################################
+# author     : linuxluemmel.ch@gmail.com                  #
+# parameters : none                                       #
+###########################################################
+
+
+cd $HOME/.xbmc/temp
+
+grep swiss-army-knife xbmc.log
+
